26d752db8dad9a1765340b105e29279dd373894a
JLockyer <jlockyerfit24@gmail.com>
2016-04-24 17:41:01 -0400

    initial setup commit

---
+++ MVC_Wardrobe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Wardrobe.MvcApplication" Language="C#" %>
---
+++ MVC_Wardrobe/Models/Accessories.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Wardrobe.Models
+{
+    public class Accessories
+    {
+        // These lines of code are just the input for the user in the webpage
+        [Key]
+        public int ID { get; set; }
+        public string Nickname { get; set; }
+        public string Photo { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+        public string Season { get; set; }
+        public string Occasion { get; set; }
+    }
+}
